572510ef2cd535865d721ee729ae63880d1294fe
manuel <me@example.com>
2020-04-17 10:41:21 -0700

    css mvp done

--- index.js
+++ index.js
@@ -200,19 +200,21 @@ const artists = [
       "paintings": 328
     }
 ]
-
+console.clear()
 // 🖌🖼 M V P 🖼🖌 //
 
 /* Task 1: Practice accessing data above by console.log-ing following items:
 
 (1) Name of the first artist in the array
 (2) Bio of the third artist in the array */
-
-
-
+console.log(artists[1].name);
+console.log(artists[3].bio);
+console.clear()
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
+console.log(artists[8].name);
+artists[8].name = 'Vincent Van Gogh';
+console.log(artists[8].name);
+console.clear()
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -222,10 +224,12 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
+function getArtistByIndex(arr, id) {
+    return `The artist at index ${id} is ${arr[id].name}.`
   }
   
+console.log(getArtistByIndex(artists, 0));
+console.clear()
   /**
 
 
@@ -237,10 +241,11 @@ function getArtistByIndex(id, name) {
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset.
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(arr, id) {
+    return arr.splice(id, 1)
   }
-  
+removeArtist(artists, 0);
+console.log(artists);
   /**
 
 
